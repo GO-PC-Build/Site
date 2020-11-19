769b925a29dfd9b6640ed3c4bb1bf479e33a27f5
Arthurdw <arthur.dewitte@gmail.com>
2020-11-19 20:14:34 +0100

    Maybe this fixes produc?

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { getCookie } from "./utils/CookieInteraction";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV !== "production"
-    ? process.env.REACT_APP_BACKEND_URL_DEV
-    : process.env.REACT_APP_BACKEND_URL;
+    ? "http://localhost:8080/go-pc-build/api/"
+    : "https://api.xiler.net/go-pc-build/api/";
 
 axios.defaults.headers.common["auth-token"] = getCookie("token");
 axios.defaults.headers.common["Content-Type"] = "application/json";
